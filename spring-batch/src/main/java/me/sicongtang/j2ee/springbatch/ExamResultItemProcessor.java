73c40a6b241d08398c7e3804600d2639551b52f7
BobbyTang <bobby3239@gmail.com>
2015-01-19 02:09:44 +0800

    spring-batch example first commit

---
+++ spring-batch/src/main/java/me/sicongtang/j2ee/springbatch/ExamResultItemProcessor.java
@@ -0,0 +1,23 @@
+package me.sicongtang.j2ee.springbatch;
+
+import org.springframework.batch.item.ItemProcessor;
+
+public class ExamResultItemProcessor implements ItemProcessor<ExamResult, ExamResult>{
+
+	
+	@Override
+	public ExamResult process(ExamResult result) throws Exception {
+		System.out.println("Processing result :"+result);
+		
+		/*
+		 * Only return results which are more than 75%
+		 * 
+		 */
+		if(result.getPercentage() < 75){
+			return null;
+		}
+		
+		return result;
+	}
+
+}
